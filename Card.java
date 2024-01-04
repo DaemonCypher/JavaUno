db67c780aa29981be6fb1c1f5dc0658aef157618
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2024-01-04 13:24:13 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,29 +1,46 @@
-public class Card {
-    // Instance variables for color and type
-    private String color;
-    private String type;
+public class Card
+{
 
-    // Constructor to initialize the card
-    public Card(String color, String type) {
-        this.color = color;
-        this.type = type;
-    }
+  // Instance variables for color and type
+  private String color;
+  private String type;
+  private String chosenColor;	// For wild cards
 
-    // Getter for color
-    public String getColor() {
-        return color;
-    }
+  // Constructor to initialize the card
+  public Card (String color, String type)
+  {
+    this.color = color;
+    this.type = type;
+  }
 
-    // Getter for type
-    public String getType() {
-        return type;
-    }
-    @Override
-    public String toString() {
-        return "Card{" + "color='" + color + '\'' + ", type='" + type + '\'' + '}';
-    }
-    public void setColor(String newcolor)
-    {
-        this.color = newcolor;
-    }
+  public void setChosenColor (String chosenColor)
+  {
+    this.chosenColor = chosenColor;
+  }
+  public String getChosenColor ()
+  {
+    return chosenColor;
+  }
+
+  // Getter for color
+  public String getColor ()
+  {
+    return color;
+  }
+
+  // Getter for type
+  public String getType ()
+  {
+    return type;
+  }
+
+  @Override public String toString ()
+  {
+    return "Card{" + "color='" + color + '\'' + ", type='" + type + '\'' +'}';
+  }
+
+  public void setColor (String newcolor)
+  {
+    this.color = newcolor;
+  }
 }
