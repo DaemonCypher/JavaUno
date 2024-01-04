db91e4359b326a64f3c54d22c9145aa2d1cc4486
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2024-01-04 13:24:51 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,43 +1,65 @@
 import java.util.*;
 
-public class Deck {
-    private Stack<Card> deck;
+public class Deck
+{
+  private Stack < Card > deck;
 
-    enum Color { RED, YELLOW, BLUE, GREEN }
-    enum Type { ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE, PLUS2, WILD, WILDPLUS4 }
+  enum Color
+  { RED, YELLOW, BLUE, GREEN, ANY }
+  enum Type
+  { ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE,
+    PLUS2, WILD, WILDPLUS4
+  }
 
-    public Deck() {
-        deck = new Stack<>();
-        initializeDeck();
+  public Deck ()
+  {
+    deck = new Stack <> ();
+    initializeDeck ();
+  }
+
+  private void initializeDeck ()
+  {
+    // Add regular cards
+    for (Color color:Color.values ())
+    {
+
+	    if (color != Color.ANY)
+	    {			// Exclude 'ANY' for regular cards
+	        for (Type type:Type.values ())
+	        {
+
+		        if (!type.equals (Type.WILD) && !type.equals (Type.WILDPLUS4))
+		        {
+                    deck.push (new Card (color.name (), type.name ()));
+                    deck.push (new Card (color.name (), type.name ()));	// Add a second card of the same type for each color
+		        }
+
+	        }
+	    }
+
     }
-
-    private void initializeDeck() {
-        for(Color color : Color.values()) {
-            for(Type type : Type.values()) {
-                // Add one card of each type for each color
-                deck.push(new Card(color.name(), type.name()));
-
-                // If not a wild card, add a second card of the same type for each color
-                /*if (!type.equals(Type.WILD) && !type.equals(Type.WILDPLUS4)) {
-                    deck.push(new Card(color.name(), type.name()));
-                }
-                */
-            }
-        }
-
-        // Shuffle the deck
-        Collections.shuffle(deck);
+    // Add wild cards
+    for (int i = 0; i < 4; i++)
+    {				
+	    deck.push (new Card (Color.ANY.name (), Type.WILD.name ()));
+	    deck.push (new Card (Color.ANY.name (), Type.WILDPLUS4.name ()));
     }
+    // Shuffle the deck
+    Collections.shuffle (deck);
+  }
 
-    public boolean isEmpty() {
-        return deck.isEmpty();
-    }
+  public boolean isEmpty ()
+  {
+    return deck.isEmpty ();
+  }
 
-    public Card draw() {
-        if (!deck.isEmpty()) {
-            return deck.pop();
-        }
-        return null; // or throw an exception if the deck is empty
+  public Card draw ()
+  {
+    if (!deck.isEmpty ())
+    {
+	    return deck.pop ();
     }
+    return null;		// or throw an exception if the deck is empty
+  }
 
 }
